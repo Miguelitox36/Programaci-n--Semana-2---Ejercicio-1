9c8e2357eb3fcfc3e7707765198dad17e91d3a54
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:27:37 -0500

    Clase Abstracta Projectile

---
+++ Semana 2 - Ejercicio 1/Semana 2 - Ejercicio 1/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2___Ejercicio_1
+{
+    public abstract class Projectile
+    {
+        public int Damage { get; set; }
+    }
+}
