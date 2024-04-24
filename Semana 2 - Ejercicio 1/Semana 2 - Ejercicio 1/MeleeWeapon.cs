7d6e6422dcf21ceb43b090e5848795486aecc9af
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:28:50 -0500

    Clase MeleeWeapon

---
+++ Semana 2 - Ejercicio 1/Semana 2 - Ejercicio 1/MeleeWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2___Ejercicio_1
+{
+    public class MeleeWeapon : IWeapon
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public float AttackSpeed { get; set; }
+
+        public float DPS()
+        {
+            return Damage * AttackSpeed;
+        }
+    }
+}
