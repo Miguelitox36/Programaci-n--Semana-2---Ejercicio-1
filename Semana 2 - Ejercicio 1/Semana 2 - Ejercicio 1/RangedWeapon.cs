155763524311347539a6609b0987fb1c32c68364
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:29:48 -0500

    Clase RangedWeapon

---
+++ Semana 2 - Ejercicio 1/Semana 2 - Ejercicio 1/RangedWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2___Ejercicio_1
+{
+    public class RangedWeapon : IWeapon
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public float AttackSpeed { get; set; }
+        public Projectile Projectile { get; set; }
+
+        public float DPS()
+        {
+            return (Damage + Projectile.Damage) * AttackSpeed;
+        }
+    }
+}
