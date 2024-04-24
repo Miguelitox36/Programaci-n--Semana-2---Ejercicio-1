174de251051fbb335d30c49ff889c8956b1f84bd
Miguelitox36 <miguelitox36@hotmail.com>
2024-04-23 19:26:49 -0500

    Interface IWeapon

---
+++ Semana 2 - Ejercicio 1/Semana 2 - Ejercicio 1/IWeapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_2___Ejercicio_1
+{
+    public interface IWeapon
+    {
+        string Name { get; }
+        int Damage { get; }
+        float AttackSpeed { get; }
+        float DPS();
+    }
+}
